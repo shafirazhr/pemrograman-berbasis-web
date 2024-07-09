a27e15805472ff17a06c5b6803b688f4a4da137a
shafirazhr <shafirazhr28@gmail.com>
2024-07-09 10:01:47 +0700

    Tugas project matkul

---
+++ tugasmatkul/tugas/shafira.py
@@ -0,0 +1,23 @@
+def fungsi_python():
+ print ("hai ini adalah python")
+
+nama = "shafira"
+umur = 18
+print("nama saya adalah " + nama + " umur saya adalah "+str(umur))
+print("umur saya adalah : ", umur)
+
+
+# List <Array>
+hewan = ["ayam", "kucing", "kelinci", "tupai"]
+print(hewan)
+
+
+fungsi_python()
+# INI KOMENTAR
+# INI BUKAN KOMENTAR
+# CTRL + /
+
+angka1 = 5
+angka2 = 10
+angka3 = 15
+print ("penjumlahan : " + 
